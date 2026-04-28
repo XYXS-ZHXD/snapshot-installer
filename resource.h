25f1a67722c9ea2a6c70b4b244f6a1f211ad7785
相由心生 <287835247@qq.com>
2026-04-28 15:00:50 +0800

    Add resource.h (was accidentally removed)

---
+++ resource.h
@@ -0,0 +1,17 @@
+#ifndef RESOURCE_H
+#define RESOURCE_H
+
+/* Icon */
+#define IDI_APP             101
+
+/* Controls */
+#define IDC_SNA_PATH       1001
+#define IDC_BTN_BROWSE     1002
+#define IDC_TARGET_DRIVE   1003
+#define IDC_PASSWORD       1004
+#define IDC_BTN_INSTALL    1005
+#define IDC_STATUS         1006
+#define IDC_DRIVE_LIST     1007
+#define IDC_CHK_FIXBOOT    1008
+
+#endif
